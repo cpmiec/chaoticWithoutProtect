d7e6bb79444a8d1cd2f63d0aa25a29f7591e676c
cpmiec <cpmiec@live.com>
2018-12-23 20:43:57 +0800

    Initialize

---
+++ CWChaoticWithoutProtect/CWChaoticWithoutProtect/Debug/src/maskedChaoticEnc.d
@@ -0,0 +1,10 @@
+src/maskedChaoticEnc.d src/maskedChaoticEnc.o: ../src/maskedChaoticEnc.c \
+ ../src/maskedChaoticEnc.h \
+ d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h
+
+../src/maskedChaoticEnc.h:
+
+d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
